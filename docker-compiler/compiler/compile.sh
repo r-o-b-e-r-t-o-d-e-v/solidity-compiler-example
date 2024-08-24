390a9276ebbacd08e1fe474f0763db40b75f995a
Roberto <robertomdev@gmail.com>
2024-08-24 16:15:08 +0200

    * Removed script for renaming compile generated files. It seems the compiler is generating the names in the proper way without the need of running the script. * Added params to the compile-contracts.sh script to set where the sources files are coming from and where the output is expected to be generated. * Also added a few params for the compile.sh script: contracts and output location and a couple for the output files packing functionality.

--- docker-compiler/compiler/compile.sh
+++ docker-compiler/compiler/compile.sh
@@ -1,5 +1,65 @@
 #!/bin/sh
 
+# Accepted args:
+# --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---
+#
+# -c <contracts_folder>   Param to set the location of the contracts.
+#
+#                         If not specified, the script will search for
+#                         a so called 'contracts' folder in all the
+#                         subdirectories from the execution context.
+#
+#                         This arg expects the path to be related to
+#                         the execution context.
+#
+# -o <output_folder>      Param to set the location of the output
+#                         folder the compile files will be generated at.
+#
+#                         If not specified, the script will search for
+#                         a so called 'output' folder in all the
+#                         subdirectories from the execution context.
+#                         If none is found, it will use the 'contracts'
+#                         folder to generate the output files there
+#                         (.../contracts/output/*).
+#
+#                         This arg expects the path to be related to
+#                         the execution context.
+#
+# --pack                  Flag to tell the script to pack the compiled
+#                         files so every smart contract just have a
+#                         single .json file containing both the abi
+#                         and the bin. Otherwise each contract would
+#                         have several files (.bin, .abi and _meta).
+#
+# --keep-unpacked         Flag to tell the script if when packing
+#                         the compiled files should also keep the
+#                         original ones.
+#                         **Requires --pack flag to also be set.
+#
+# --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---
+
+
+# Constants
+# --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---
+SCRIPT_DIR=$(dirname "$(realpath "$0")")
+
+# Establish a default directory for contracts by finding any folder with that name in subdirectories
+CONTRACTS_DIR_DEFAULT=$(find $(pwd) -type d -name 'contracts' -print)
+CONTRACTS_DIR="$CONTRACTS_DIR_DEFAULT"
+
+# ---  .  ---  .  ---  .  ---  .  ---  .  ---
+
+# Establish a default directory for output by finding any folder with that name in subdirectories
+OUTPUT_DIR_DEFAULT=$(find $(pwd) -type d -name 'output' -print)
+
+# If no output folder exists, creates a default path based on the contracts folder location
+if [ -z "$OUTPUT_DIR_DEFAULT" ]; then
+    OUTPUT_DIR_DEFAULT="$CONTRACTS_DIR_DEFAULT/output"
+fi
+
+OUTPUT_DIR="$OUTPUT_DIR_DEFAULT"
+# --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---
+
 
 # ARGS
 # --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---
@@ -16,12 +76,54 @@ while [ $# -gt 0 ]; do
       KEEP_UNPACKED_FILES="true"
       shift
       ;;
+    -c)
+      if [ -n "$2" ]; then
+        CONTRACTS_DIR_RELATIVE_PATH="$2"
+        shift 2
+      else
+        echo "Error: -c requires a non-empty option argument."
+        exit 1
+      fi
+      ;;
+    -o)
+      if [ -n "$2" ]; then
+        OUTPUT_DIR_RELATIVE_PATH="$2"
+        shift 2
+      else
+        echo "Error: -o requires a non-empty option argument."
+        exit 1
+      fi
+      ;;
     *)
       echo "Unknown argument: $1"
       exit 1
       ;;
   esac
 done
+# --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---
+
+
+# Solving contracts directory
+# --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---
+if [ -z "$CONTRACTS_DIR_RELATIVE_PATH" ]; then
+  echo "No contracts directory was set. Using default one: $CONTRACTS_DIR_DEFAULT"
+else
+  CONTRACTS_DIR="$(pwd)/${CONTRACTS_DIR_RELATIVE_PATH#./}"
+  echo "CONTRACTS_DIR_RELATIVE_PATH:: $CONTRACTS_DIR_RELATIVE_PATH"
+  echo "Set contracts directory to: $CONTRACTS_DIR"
+fi
+# --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---
+
+
+# Solving output directory
+# --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---
+if [ -z "$OUTPUT_DIR_RELATIVE_PATH" ]; then
+  echo "No output directory was set. Using default one: $OUTPUT_DIR_DEFAULT"
+else
+  OUTPUT_DIR="$(pwd)/${OUTPUT_DIR_RELATIVE_PATH#./}"
+  echo "OUTPUT_DIR_RELATIVE_PATH:: $OUTPUT_DIR_RELATIVE_PATH"
+  echo "Set output directory to: $OUTPUT_DIR"
+fi
 # --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---
 
 
@@ -34,21 +136,15 @@ fi
 # --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---
 
 
+rm -rf "$OUTPUT_DIR"
+
+
 # Running the script to do the actual contracts compilation
 # --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---
-$SCRIPT_DIR/scripts/compile-contracts.sh
+$SCRIPT_DIR/scripts/compile-contracts.sh -c "$CONTRACTS_DIR" -o "$OUTPUT_DIR"
 if [ $? -ne 0 ]; then
   exit 1
 fi
-# --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---
-
-
-# Running the script to normalize the name of the compiled contracts
-# The reason for this is because solc generates filenames differently
-# depending on whether you compiled a single contract or multiple ones
-# in the same compilation process.
-# --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---
-$SCRIPT_DIR/scripts/rename-contracts.sh
 # --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---
 
 
--- docker-compiler/compiler/scripts/compile-contracts.sh
+++ docker-compiler/compiler/scripts/compile-contracts.sh
@@ -1,6 +1,70 @@
 #!/bin/sh
 
 
+# Accepted args:
+# --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---
+#
+# -c <contracts_folder>   Param to set the location of the contracts.
+#                         If not specified, the script will exit.
+#
+#                         This arg expects the path to be absolute,
+#                         no matter the execution context.
+#
+# -o <output_folder>      Param to set the location of the output
+#                         folder the compile files will be generated at.
+#
+#                         If not specified, the script will exit.
+#                         This arg expects the path to be absolute,
+#                         no matter the execution context.
+#
+# --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---
+
+
+# ARGS
+# --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---
+while [ $# -gt 0 ]; do
+  case "$1" in
+    -c)
+      if [ -n "$2" ]; then
+        SOURCES_DIR="$2"
+        shift 2
+      else
+        echo "Error: -o requires a non-empty option argument."
+        exit 1
+      fi
+      ;;
+    -o)
+      if [ -n "$2" ]; then
+        OUTPUT_DIR="$2"
+        shift 2
+      else
+        echo "Error: -o requires a non-empty option argument."
+        exit 1
+      fi
+      ;;
+    *)
+      echo "Unknown argument: $1"
+      exit 1
+      ;;
+  esac
+done
+# --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---
+
+
+# Args validation
+# --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---
+if [ -z "$SOURCES_DIR" ]; then
+  echo "No sources directory was specified"
+  exit 1
+fi
+
+if [ -z "$OUTPUT_DIR" ]; then
+  echo "No output directory was specified ($0)"
+  exit 1
+fi
+# --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---
+
+
 # Quick checking to make sure Docker is running
 # --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---
 if ! docker info > /dev/null 2>&1; then
@@ -14,7 +78,7 @@ fi
 # This is basically to add several contracts to be compiled at the same time
 # --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---
 metadata_args=""
-for file in $(pwd)/contracts/*.sol; do
+for file in "$SOURCES_DIR"/*.sol; do
   metadata_args+=" /sources/$(basename "$file")"
 done
 # --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---
@@ -23,13 +87,14 @@ done
 # Run the Docker container, compile the contracts and remove the container
 # --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---
 docker run --rm \
--v $(pwd)/contracts:/sources \
+-v "$SOURCES_DIR":/sources \
+-v "$OUTPUT_DIR":/output \
 ethereum/solc:0.8.26-alpine \
 --optimize \
 --bin \
 --abi \
 --metadata \
 $metadata_args \
--o /sources/output/ \
+-o /output/ \
 --overwrite
 # --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---
