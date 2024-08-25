15043dcd47e41826bdfbcda6aac6840e7146ca11
Roberto <robertomdev@gmail.com>
2024-08-25 11:50:03 +0200

    * [js-compiler] Small refactor on compile.js

--- js-compiler/compiler/compile.js
+++ js-compiler/compiler/compile.js
@@ -13,7 +13,7 @@ const pack = hasFlag(args, '--pack');
 const keepUnpacked = hasFlag(args, '--keep-unpacked');
 
 if (!pack && keepUnpacked) {
-    console.log("Flag --keep-unpacked can only be used along with --pack");
+    console.error("Flag --keep-unpacked can only be used along with --pack");
     process.exit(1);
 }
 // --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---
@@ -21,28 +21,18 @@ if (!pack && keepUnpacked) {
 
 // Handle contracts and output location path params
 // --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---
-const {contractsDir, outputDir} = solveSourcesAndOutput(args)
+const {contractsDir, outputDir} = solveSourcesAndOutputArgs(args)
 // --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---
 
 
 // Get all Solidity file names from the sources directory.
 // --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---
-let contractFileNames;
 
 // If the directory doesn't exist, it stops the execution of the script
-try {
-    contractFileNames = fs.readdirSync(contractsDir).filter(file => file.endsWith('.sol'));
-} catch (err) {
-    if (err.code === 'ENOENT') {
-        console.error(`Error: Directory '${contractsDir}' does not exist.`);
-    } else {
-        console.error('An unexpected error occurred:', err.message);
-    }
-    process.exit(1);
-}
+const contractFileNames = getSolFileNamesFrom(contractsDir)
 
 // If the directory has no Solidity files, no sources to compile, hence it finishes the execution of the script
-if(contractFileNames.length === 0){
+if (contractFileNames.length === 0) {
     console.log(`No contracts were found at ${contractsDir}`);
     process.exit(0);
 }
@@ -81,9 +71,12 @@ const output = JSON.parse(solc.compile(input));
 
 // Prepare the output directory
 // --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---
+
+// If the output directory already exists, it removes the current content
 if (fs.existsSync(outputDir)) {
     fs.rmSync(outputDir, {recursive: true, force: true});
 }
+// Create the output dir
 fs.mkdirSync(outputDir, {recursive: true});
 
 const shouldKeepOriginals = !pack || keepUnpacked
@@ -91,39 +84,43 @@ const shouldKeepOriginals = !pack || keepUnpacked
 // Loop through each contract in the output
 for (const contractFileName in output.contracts) {
     const contracts = output.contracts[contractFileName];
-    const contractName = Object.keys(contracts)[0];
-    const contractDetails = Object.values(contracts)[0];
+    const contract = Object.values(contracts)[0]
 
-    const abi = contractDetails.abi;
-    const bytecode = contractDetails.evm.bytecode.object;
-
-    if (shouldKeepOriginals) {
-        fs.writeFileSync(path.join(outputDir, `${contractName}_abi.json`), JSON.stringify(abi, null, 2));
-        fs.writeFileSync(path.join(outputDir, `${contractName}_bytecode.json`), JSON.stringify(bytecode, null, 2));
-    }
-
-    if (pack) {
-        let packedDir;
-        if (keepUnpacked) {
-            packedDir = path.join(outputDir, 'packed')
-        } else {
-            packedDir = outputDir
-        }
-        if (!fs.existsSync(packedDir)) {
-            fs.mkdirSync(packedDir, {recursive: true});
-        }
-        fs.writeFileSync(path.join(packedDir, `${contractName}.json`), JSON.stringify({
-            packingVersion: '1.0.0',
-            abi: abi,
-            bin: bytecode,
-        }, null, 2));
-    }
+    generateOutputFiles(
+        {
+            contractName: Object.keys(contracts)[0],
+            abi: contract.abi,
+            bytecode: contract.evm.bytecode.object
+        },
+        outputDir,
+        {
+            pack: pack,
+            shouldKeepOriginals: shouldKeepOriginals
+        });
 }
 
 console.log(`ABI and bytecode exported to ${outputDir}`);
 // --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---
 
-function solveSourcesAndOutput(args) {
+
+// Helper functions
+// --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---
+function getSolFileNamesFrom(contractsDir) {
+    let contractFileNames = [];
+    try {
+        contractFileNames = fs.readdirSync(contractsDir).filter(file => file.endsWith('.sol'));
+    } catch (err) {
+        if (err.code === 'ENOENT') {
+            console.error(`Error: Directory '${contractsDir}' does not exist.`);
+        } else {
+            console.error('An unexpected error occurred:', err.message);
+        }
+        process.exit(1);
+    }
+    return contractFileNames
+}
+
+function solveSourcesAndOutputArgs(args) {
     const currentContext = process.cwd()
 
     const paramContractsDir = getParamValue(args, '-c')
@@ -139,5 +136,34 @@ function solveSourcesAndOutput(args) {
         outputDir = path.join(currentContext, paramOutputDir);
     }
 
-    return { contractsDir: contractsDir, outputDir: outputDir }
+    return {contractsDir: contractsDir, outputDir: outputDir}
 }
+
+function generateOutputFiles(contractInfo, outputDir, packingOptions) {
+    const {contractName, abi, bytecode} = contractInfo
+    const {pack, shouldKeepOriginals} = packingOptions
+
+    if (shouldKeepOriginals) {
+        fs.writeFileSync(path.join(outputDir, `${contractName}_abi.json`), JSON.stringify(abi, null, 2));
+        fs.writeFileSync(path.join(outputDir, `${contractName}_bytecode.json`), JSON.stringify(bytecode, null, 2));
+    }
+
+    if (pack) {
+        writeIntoPackedDir(keepUnpacked, outputDir, contractName, JSON.stringify({
+            packingVersion: '1.0.0',
+            abi: abi,
+            bin: bytecode,
+        }, null, 2));
+    }
+}
+
+function writeIntoPackedDir(keepUnpacked, outputDir, contractName, content) {
+    const packedDir = path.join(outputDir, keepUnpacked ? 'packed' : '')
+
+    if (!fs.existsSync(packedDir)) {
+        fs.mkdirSync(packedDir, {recursive: true});
+    }
+
+    fs.writeFileSync(path.join(packedDir, `${contractName}.json`), content);
+}
+// --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---
